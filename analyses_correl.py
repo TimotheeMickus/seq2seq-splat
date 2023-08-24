b0ba1ba78ab0475f0e7a1c7a7070d34974e49bf4
jrvc <jraulvc@gmail.com>
2023-08-24 13:45:28 +0300

    Correlations with comet,bleu&chrf + bug fixes

--- analyses_correl.py
+++ analyses_correl.py
@@ -10,39 +10,39 @@ from utils import read_datasets
 
 
 
-def main(data,multilingual=False,oh_decomp=False):
+def main(data, multilingual=False, oh_decomp=False, qualitymetric='bleu'):
     metadata, datadict = read_datasets(data, multilingual,oh_decomp=oh_decomp)
     components = ['I','S','T','F','C'] if not(oh_decomp) else ['S','T','C']
     name=''.join(components)+'_L1norm'
     #infer = ['gen','no-gen'] if metadata['generative']=='both' else [metadata['generative']]
-    bleu = pd.read_csv(f'results/bleu-scores2.csv').sort_values('checkpoint')
-    bleu = bleu.set_index('checkpoint').sort_index()
+    qscores = pd.read_csv(f'results/{qualitymetric}-scores2.csv').sort_values('checkpoint')
+    qscores = qscores.set_index('checkpoint').sort_index()
 
     rng = np.random.default_rng()
     samplesize = 1000
-
+    nexperims=10
     spearmans = None
-    for _ in range(10):
+    for _ in range(nexperims):
         for m1,pd1 in datadict.items():
-            diffDF=pd.DataFrame(columns=['checkpoint', 'checkpoint2','layer_idx','func','bleu',name]+components)
+            diffDF=pd.DataFrame(columns=['checkpoint', 'checkpoint2','layer_idx','func',qualitymetric,name]+components)
             m2 = f'{m1}-eng' if len(m1)>2 else f'rus-eng_{m1}'
-            for infer in bleu.infer_style.unique():
-                bleudiff,funcdiff = [],[]
-                bleupart = bleu[bleu.model==m2]
-                ckpts = rng.integers(1,bleupart.index.max()//1000, size=2)*1000
+            for infer in qscores.infer_style.unique():
+                qscoresdiff,funcdiff = [],[]
+                qscorespart = qscores[qscores.model==m2]
+                ckpts = rng.integers(1,qscorespart.index.max()//1000, size=2)*1000
                 #for layer,func,comp in itertools.product(range(1,7),pd1[infer].func.unique(),['I','S','T','F','C']):
                 for func in pd1[infer].func.unique():
                     #ts  =pd1[infer][(pd1[infer].func==func)& (pd1[infer].layer_idx==layer) ].set_index('checkpoint').drop('func',axis=1) #[comp]
                     #ts  =pd1[infer][(pd1[infer].layer_idx==layer) ].set_index('checkpoint')#[comp]
                     ts  =pd1[infer][(pd1[infer].func==func) ].set_index('checkpoint').drop('func',axis=1) #[comp]
-                    maxckpt = np.min((bleupart.index.max(),ts.index.max()))//1000
+                    maxckpt = np.min((qscorespart.index.max(),ts.index.max()))//1000
                     for ck1,ck2 in random.sample(list(itertools.combinations(range(1,1+maxckpt),2)),samplesize):
                         ckpts = [ck1*1000,ck2*1000] 
-                        coso = bleupart.bleu.loc[ckpts].diff().dropna().abs()
+                        coso = qscorespart.score.loc[ckpts].diff().dropna().abs()
                         diffs = ts.loc[ckpts].sort_values(by='layer_idx').diff().dropna().abs()
                         diffs = diffs[diffs.layer_idx == 0]
                         diffs.layer_idx = [1,2,3,4,5,6]
-                        diffs['bleu'] = coso
+                        diffs[qualitymetric] = coso
                         diffs['func'] = func
                         diffs = diffs.reset_index()
                         diffs['checkpoint2'] = ckpts[0]
@@ -50,12 +50,12 @@ def main(data,multilingual=False,oh_decomp=False):
                         # TODO: compute them also individually
                         diffs[name]=diffs[components].abs().sum(axis=1)
                         diffDF = pd.concat([diffDF,diffs])
-                        #bleudiff.append(coso.loc[ckpts[1]])
+                        #qscoresdiff.append(coso.loc[ckpts[1]])
                         #funcdiff.append(diffs.ISFTCmeanvec.values)
             diffDF = diffDF.reset_index()
-            print(f'MODEL {m1}: Spearman correlations between Δ(BLEU) and {[name]+components} for a sample of {samplesize} pairs of ckpts')
+            print(f'MODEL {m1} experim{_}: Spearman correlations between Δ({qualitymetric}) and {[name]+components} for a sample of {samplesize} pairs of ckpts')
             for column in [name]+components:
-                aux = diffDF.groupby(['layer_idx','func'])[['bleu',column]]
+                aux = diffDF.groupby(['layer_idx','func'])[[qualitymetric,column]]
                 colname = column+' '+m1+' '+str(_)
                 if spearmans is None:
                     spearmans = aux.corr('spearman').iloc[0::2,-1].sort_index(level=1).droplevel(level=2).reset_index()
@@ -73,18 +73,25 @@ def main(data,multilingual=False,oh_decomp=False):
 
     cols = [col for col in spearmans if re.search(r" s[0-2] ", col)]
     for column in [name]+components:
-        spearmansSTATS[f'{column} rus_allseeds mean'] = spearmans[cols].mean(axis=1) #spearmans.apply(lambda row: np.mean([row[col] for col in cols]), axis=1)
-        spearmansSTATS[f'{column} rus_allseeds stdv'] = spearmans[cols].std(axis=1) #spearmans.apply(lambda row: np.std([row[col] for col in cols]), axis=1)
+        cc = [col for col in cols if col.startswith(column)]
+        spearmansSTATS[f'{column} rus_allseeds mean'] = spearmans[cc].mean(axis=1) 
+        spearmansSTATS[f'{column} rus_allseeds stdv'] = spearmans[cc].std(axis=1) 
     print(spearmansSTATS)
-    fsuffix="oh-schuler" if oh_decomp else "mickus-etal"
+    fsuffix=f"{qualitymetric}_oh-schuler" if oh_decomp else f"{qualitymetric}_mickus-etal"
     spearmansSTATS.to_csv(f'results/Spearmancorrelations-{fsuffix}.csv')
 
 
-sys.argv = sys.argv if len(sys.argv)>=2 else [sys.argv[0],'gen','notmultilingual','mickus']
+sys.argv = sys.argv if len(sys.argv)>3 else [sys.argv[0],'gen','notmultilingual','mickus']
 if __name__ == '__main__':
     data = sys.argv[1]
     multilingual = True if sys.argv[2]=='multilingual' else False
     oh_decomp = True if sys.argv[3].lower().find('oh') > -1 else False
 
-    main(data, multilingual,oh_decomp)
-    main(data, multilingual,not(oh_decomp))
+    #main(data, multilingual,oh_decomp)
+    #main(data, multilingual,not(oh_decomp))
+    main(data, multilingual,oh_decomp,qualitymetric='bleu')
+    main(data, multilingual,oh_decomp,qualitymetric='comet')
+    main(data, multilingual,oh_decomp,qualitymetric='chrf')
+    main(data, multilingual,not(oh_decomp),qualitymetric='bleu')
+    main(data, multilingual,not(oh_decomp),qualitymetric='comet')
+    main(data, multilingual,not(oh_decomp),qualitymetric='chrf')
